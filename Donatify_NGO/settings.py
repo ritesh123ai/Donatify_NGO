237127054861686eab5ed3b9d9baee479265a74a
Ritesh Mishra <riteshmishraedu@gmail.com>
2025-08-22 12:11:44 +0530

    Update settings.py

--- Donatify_NGO/settings.py
+++ Donatify_NGO/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_ORIGIN', 'https://donatify-ngo-4.onrender.com')]
+CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_ORIGIN', 'https://donatify-ngo-5.onrender.com')]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
